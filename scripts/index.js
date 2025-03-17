8ff77855272e95029a21bacf45e5ad523e0c7bde
Jorgend <Fullsteck@yandex.kz>
2025-03-18 02:13:20 +0500

    0.0.2

--- scripts/index.js
+++ scripts/index.js
@@ -2,41 +2,34 @@
 const templateCard = document.querySelector('#card-template').content;
 const cardsList = document.querySelector('.places__list');
 // @todo: DOM узлы
-function SplittingObjectsIntoArrays() {
-
-  const cardsSrc = initialCards.map(item => item.link);
-  const cardsAlt = initialCards.map(item => item.name);
-  const cardsText = initialCards.map(item => item.name);
-
-  return { cardsSrc, cardsAlt, cardsText };
-}
-const threeArrays = SplittingObjectsIntoArrays();
 // @todo: Функция создания карточки 
-function createCard(threeArrays, DeleteCard) {
-  for (let i = 0; i < initialCards.length; i++) {
 
-    const card = templateCard.querySelector('.card').cloneNode(true);
-    const cardDeleteButton = card.querySelector('.card__delete-button');
-    const cardImage = card.querySelector('.card__image');
-    const cardTitle = card.querySelector('.card__title');
+function createCard(dataCard, deleteCard) {
+  const card = templateCard.querySelector('.card').cloneNode(true);
+  const cardDeleteButton = card.querySelector('.card__delete-button');
+  const cardImage = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
 
-    cardImage.src = threeArrays.cardsSrc[i];
-    cardImage.alt = threeArrays.cardsAlt[i];
-    cardTitle.textContent = threeArrays.cardsText[i];
+  cardImage.src = dataCard.link;
+  cardImage.alt = dataCard.name;
+  cardTitle.textContent = dataCard.name;
 
-    cardsList.append(card);
-
-    cardDeleteButton.addEventListener('click', DeleteCard);
-  }
+  cardDeleteButton.addEventListener('click', deleteCard);
+  return card;
 };
 
 // @todo: Функция удаления карточки
-const DeleteCard = function (event) {
+
+const deleteCard = function (event) {
   event.target.closest('.card').remove();
 };
 
 // @todo: Вывести карточки на страницу
-createCard(threeArrays, DeleteCard)
+
+initialCards.forEach((card, index) => {
+  console.log(`Карточка ${index + 1}:`, card);
+  cardsList.append(createCard(card, deleteCard))
+});
 
 
 
@@ -54,5 +47,4 @@ createCard(threeArrays, DeleteCard)
 
 
 
-
-//Я знаю, что можно было и получше, тут много чего можно оптимизировать, но страшно, что не пройду по чек-листу.
+//мне очень стыдно за то что я не слышал ваши коментарии(. надеюсь я исправился
