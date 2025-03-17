6cb62ec6e2ad25c1a7b3ec2726ccc8fcb6b7245e
Jorgend <Fullsteck@yandex.kz>
2025-03-17 07:09:23 +0500

    0.0.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,58 @@
 // @todo: Темплейт карточки
-const templateCard = document.querySelector('#card-template').content; // Шаблон карточки
-const cardsList = document.querySelector('.places__list'); // Список карточек
-const createCardButton = document.querySelector('.profile__add-button'); // Кнопка создания карточки
+const templateCard = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
 // @todo: DOM узлы
+function SplittingObjectsIntoArrays() {
+
+  const cardsSrc = initialCards.map(item => item.link);
+  const cardsAlt = initialCards.map(item => item.name);
+  const cardsText = initialCards.map(item => item.name);
+
+  return { cardsSrc, cardsAlt, cardsText };
+}
+const threeArrays = SplittingObjectsIntoArrays();
 // @todo: Функция создания карточки 
-let clickList = 0;
-function createCard() {
-  const card = templateCard.querySelector('.card').cloneNode(true); // Карточка
-  const cardImage = card.querySelector('.card__image'); // Изображение карточки
-  const cardTitle = card.querySelector('.card__title'); // Заголовок or text карточки
-  const deleteCardButton = card.querySelector('.card__delete-button');
-  cardImage.src = initialCards[clickList].link;
-  cardImage.alt = initialCards[clickList].name;
-  cardTitle.textContent = initialCards[clickList].name;
-  clickList += 1;
-  cardsList.append(card);
-  deleteCardButton.addEventListener('click', constDeleteCard);
-}
-// @todo: Функция удаления карточки
-const constDeleteCard = function (event) {
-  event.target.closest('.card').remove();
-  clickList -= 1;
+function createCard(threeArrays, DeleteCard) {
+  for (let i = 0; i < initialCards.length; i++) {
+
+    const card = templateCard.querySelector('.card').cloneNode(true);
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+
+    cardImage.src = threeArrays.cardsSrc[i];
+    cardImage.alt = threeArrays.cardsAlt[i];
+    cardTitle.textContent = threeArrays.cardsText[i];
+
+    cardsList.append(card);
+
+    cardDeleteButton.addEventListener('click', DeleteCard);
+  }
 };
+
+// @todo: Функция удаления карточки
+const DeleteCard = function (event) {
+  event.target.closest('.card').remove();
+};
+
 // @todo: Вывести карточки на страницу
-for (let i = 0; i < initialCards.length; i++) {
-  createCard()
-}
-createCardButton.addEventListener('click', createCard);
+createCard(threeArrays, DeleteCard)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Я знаю, что можно было и получше, тут много чего можно оптимизировать, но страшно, что не пройду по чек-листу.
